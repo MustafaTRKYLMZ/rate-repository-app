54d232f29b382e1484d76cc37bbf28686012dfab
Mustafa <cana66331@gmail.com>
2020-12-02 20:48:42 +0100

    exercises part10.12

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
 import { ApolloProvider } from '@apollo/react-hooks';
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
+
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log("Manifest",Constants.manifest);
   return (
     <NativeRouter>
       <ApolloProvider client={apolloClient}>
